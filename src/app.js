ca269bbe8bb0688f4822c397b010da647ecbe7a2
Alatr <dimalukianovweb@gmail.com>
2021-03-12 17:56:55 +0200

    parse

--- src/app.js
+++ src/app.js
@@ -1,30 +1,49 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as yup from 'yup';
+import axios from 'axios';
 import initView from './view.js';
 
-function isValid(data) {
-  const schema = yup.object().shape({
-    url: yup.string().required(),
-  });
-
+function isValid(url) {
+  const schema = yup.string().required().trim().url('Ссылка должна быть валидным URL');
   try {
-    schema.validateSync(value);
+    schema.validateSync(url);
     return null;
   } catch (err) {
     return err.message;
   }
 }
+function isValidRSS(xml) {
+  const parser = new DOMParser();
+  const newDocument = parser.parseFromString(xml, 'application/xml');
+  return newDocument.querySelector('rss') !== null;
+}
+const addProxy = (url) => `https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent(url)}`;
+
+const parseXML = (xml) => {
+  const parser = new DOMParser();
+  const newDocument = parser.parseFromString(xml, 'application/xml');
+  console.log(newDocument);
+
+};
 
 export default () => {
   const elements = {
     form: document.querySelector('[data-rss-form]'),
     formInput: document.querySelector('[data-rss-form] [data-rss-input]'),
+    submitBtn: document.querySelector('[data-rss-form] [data-submit-button]'),
+    formSubmitButton: document.querySelector('[data-rss-form] [data-rss-input]'),
     feedsList: document.querySelector('[data-feeds-list]'),
     postsList: document.querySelector('[data-posts-list]'),
     feedbackMessageBlock: document.querySelector('[data-feedback-block]'),
   };
 
   const state = {
+    rss: {
+      list: null,
+      processState: 'filling',
+      errors: null,
+
+    },
     form: {
       processState: 'filling',
       processError: null,
@@ -32,7 +51,7 @@ export default () => {
         url: null,
       },
       valid: true,
-      errors: {},
+      errors: null,
     },
   };
   const watchedState = initView(elements, state);
@@ -40,11 +59,43 @@ export default () => {
   elements.form.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    const data = new FormData(e.target);
-    const error = isValid(data);
-
+    const formData = new FormData(e.target);
+    const error = isValid(formData.get('url'));
     if (error) {
-
+      watchedState.form.valid = false;
+      watchedState.form.errors = error;
+      watchedState.form.processState = 'error';
+      return;
     }
+    watchedState.form.valid = true;
+    watchedState.form.errors = error;
+    watchedState.form.fields.url = formData.get('url');
+    watchedState.form.processState = 'validUrl';
+
+    watchedState.form.processState = 'sanding';
+
+    axios.get(addProxy(watchedState.form.fields.url))
+      .then((response) => {
+        watchedState.form.processState = 'filling';
+        if (!isValidRSS(response.data.contents)) throw new Error('invalidRSS');
+
+        watchedState.rss.errors = null;
+        watchedState.rss.processState = 'filling';
+
+        const data = parseXML(response.data.contents);
+      })
+
+      .catch((error) => {
+        if (!!error.isAxiosError && !error.response) {
+          watchedState.form.processState = 'networkFiled';
+          return;
+        }
+        if (error.message === 'invalidRSS') {
+          watchedState.rss.errors = 'Ресурс не содержит валидный RSS';
+          watchedState.rss.processState = 'invalid';
+          return;
+        }
+        watchedState.form.processState = 'filed';
+      });
   });
 };
--- src/view.js
+++ src/view.js
@@ -1,12 +1,75 @@
 import onChange from 'on-change';
 
-const renderForm = (form, elements) => {
-  switch (form.status) {
+const renderRss = (rss, elements) => {
+  // elements.formInput.classList.remove('is-invalid');
+  // elements.feedbackMessageBlock.classList.remove('text-success', 'text-danger')
+
+  switch (rss.processState) {
+    case 'invalid':
+      elements.formInput.classList.add('is-invalid');
+      elements.feedbackMessageBlock.classList.add('text-danger');
+      elements.feedbackMessageBlock.textContent = rss.errors;
+      elements.submitBtn.removeAttribute('disabled');
+      elements.formInput.removeAttribute('disabled');
+      break;
     case 'filling':
+      // console.log(456456);
+      // elements.formInput.classList.add('is-invalid');
+      // elements.feedbackMessageBlock.classList.add('text-danger');
+      // elements.feedbackMessageBlock.textContent = rss.errors;
+      break;
+    case 'success':
+      // console.log(456456);
+      // elements.formInput.classList.add('is-invalid');
+      // elements.feedbackMessageBlock.classList.add('text-danger');
+      // elements.feedbackMessageBlock.textContent = rss.errors;
       break;
 
     default:
-      throw Error(`Unknown form status: ${form.status}`);
+      throw Error(`Unknown rss processState: ${rss.processState}`);
+  }
+};
+
+const renderForm = (form, elements) => {
+  elements.formInput.classList.remove('is-invalid');
+  elements.feedbackMessageBlock.classList.remove('text-success', 'text-danger');
+
+  switch (form.processState) {
+    case 'error':
+      elements.formInput.classList.add('is-invalid');
+      elements.feedbackMessageBlock.classList.add('text-danger');
+      elements.feedbackMessageBlock.textContent = form.errors;
+      break;
+    case 'validUrl':
+      elements.feedbackMessageBlock.textContent = form.errors ?? '';
+      break;
+    case 'sanding':
+      elements.submitBtn.setAttribute('disabled', true);
+      elements.formInput.setAttribute('disabled', true);
+      break;
+    case 'filling':
+      elements.submitBtn.removeAttribute('disabled');
+      elements.formInput.removeAttribute('disabled');
+      break;
+    case 'filed':
+      elements.feedbackMessageBlock.textContent = 'Неизвестная ошибка';
+      elements.feedbackMessageBlock.classList.add('text-danger');
+      elements.submitBtn.removeAttribute('disabled');
+      elements.formInput.removeAttribute('disabled');
+      break;
+    case 'networkFiled':
+      elements.feedbackMessageBlock.textContent = 'Ошибка сети';
+      elements.feedbackMessageBlock.classList.add('text-danger');
+      elements.submitBtn.removeAttribute('disabled');
+      elements.formInput.removeAttribute('disabled');
+      break;
+    case 'success':
+      elements.submitBtn.removeAttribute('disabled');
+      elements.formInput.removeAttribute('disabled');
+      break;
+
+    default:
+      throw Error(`Unknown form processState: ${form.processState}`);
   }
 };
 
@@ -15,10 +78,12 @@ export default (elements, state) => {
   elements.formInput.focus();
 
   const mapping = {
-    'form.status': () => renderForm(state.form, elements),
+    'form.processState': () => renderForm(state.form, elements),
+    'rss.processState': () => renderRss(state.rss, elements),
   };
 
-  const watchedState = onChange(state, (path) => {
+  const watchedState = onChange(state, (path, value, previousValue, name) => {
+    console.log('--------', path, value, previousValue, name);
     if (mapping[path]) {
       mapping[path]();
     }
